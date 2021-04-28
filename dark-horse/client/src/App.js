111d7038c59c871678d49130ee618be6ccb50838
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-04-28 14:16:01 -0700

    inline style in html page to extend page width

--- dark-horse/client/src/App.js
+++ dark-horse/client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import LeafMap from './components/Map';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+// import LeafMap from './components/Map';
 
 function App() {
   return (
+    <Router>
     <div>
-      <LeafMap />
+      <Nav />
+      <Route exact path={["/", "/home"]} component={Home} />
+      {/* <LeafMap /> */}
     </div>
+    </Router>
   );
 }
 
