40bbc8171943d0c41b8053af2cfb55896822086b
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-04-30 17:37:56 -0700

    deleted style sheet for Nav because it wasn't being read and trending campaign because consolidated in bankcard component

--- dark-horse/client/src/components/TrendingCampaigns.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TrendingCampaigns = () => {
-    return (
-        <h1>Check out these trending campaigns</h1>
-        // get from database to dynamically populate
-        
-    )
-}
