910b7b7e0cfe297a517e21dbe8704b0dac647011
Tomara Petty <tomararuth@gmail.com>
2021-05-04 00:03:25 -0500

    added elevator pitch

--- client/src/components/CoverPhoto.js
+++ client/src/components/CoverPhoto.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import HomePhoto from "../../src/images/CoverPhoto.png";
 import "../styles/CoverPhoto.css";
+import Wrapper from './Wrapper';
 
 
 const CoverPhoto = () => {
     return (
+        <Wrapper>
+        <div className="container">
+        <h1 className="centered">Find fans | Book events | Support your favorite artists</h1>
         <img className="coverpic"src={HomePhoto} alt="Cover Photo"></img>
+        </div>
+        </Wrapper>
     )
     
 }
